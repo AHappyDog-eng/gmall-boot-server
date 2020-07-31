4ccc63c7dfe336817876615239a4b3a1813a5ac8
wangning <WangNingJava@163.com>
2020-07-31 16:26:26 +0800

    wangning

---
+++ gmall-provider-8001/src/main/java/com/mogujie/gmallprovider8001/aspect/Main.java
@@ -0,0 +1,11 @@
+package com.mogujie.gmallprovider8001.aspect;
+
+/**
+ * @author WN
+ * @date 2020/7/31 16:00
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
